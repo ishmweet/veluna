f51e13b4e65341474ed00e3899ad1109ff79e89c
corvainx <ishmeetsingharora07@gmail.com>
2026-08-29 19:39:24 +0530

    fix: enforce single-instance application lifecycle

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3485,6 +3485,14 @@ fn main() {
     init_bin_paths();
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            if let Some(w) = app.get_webview_window("main") {
+                let _ = w.show();
+                let _ = w.unminimize();
+                let _ = w.set_focus();
+                let _ = w.emit("window_focus", ());
+            }
+        }))
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
